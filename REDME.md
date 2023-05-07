3c64468b6aff82775efe5e21d653abc5448dc983
Thiago Caetano <46784384+caetanosbr@users.noreply.github.com>
2023-05-07 15:15:45 -0300

    v1

---
+++ REDME.md
@@ -0,0 +1,36 @@
+<h1 align="center"> Biscoito da Sorte </h1>
+
+<p align="center">
+  <img alt="imagem" src=./assets/TelaBiscoito.jpg>
+</p>
+
+---
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- Estrutura de dados HTML
+- Animações com CSS
+- Funções no Javascript
+- Manipulação da DOM
+- Biblioteca JS Math()
+- Funções *callback*
+- Arrays
+
+## 💻 Projeto
+
+ Jogo Biscoito da Sorte, no qual o usuário, a partir de um clique ou enter, abre um biscoito com a sua sorte do dia. 
+
+## 🔖 Layout
+
+Você pode visualizar o layout do projeto através [DESSE LINK](https://www.figma.com/file/UuvEdB2S1PjG1HZmRF0G31/Biscoito-da-Sorte-(Community)?node-id=0-1&t=uANscYB9Dr40sl8P-0) É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
+
+## : Licença
+
+Esse projeto está sob a licença MIT.
+
+---
+
+Deploy:
+[Clicque aqui para acessar o Projeto!](https://)
---
+++ script.js
@@ -0,0 +1,79 @@
+// variáveis
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnReset = document.querySelector("#btnReset")
+const open = document.querySelector("#img1")
+
+// Eventos
+open.addEventListener("click", handleClick)
+btnReset.addEventListener("click", handleClick)
+document.addEventListener("keydown", handleclick)
+
+// funções callback
+function handleClick(event) {
+  toggleScreen()
+  let randomNumber = Math.round(Math.random() * 10) 
+
+  switch (randomNumber) {
+
+    case 0:
+      screen2.querySelector("p").innerText = 
+        "Se alguém está tão cansado que não te der um sorriso, deixa-lhe o teu."
+      break
+    case 1:
+      screen2.querySelector("p").innerText = 
+        "Nem todos os dias são bons, mas há algo bom em cada dia."
+      break
+    case 2:
+      screen2.querySelector("p").innerText = 
+        "Siga confiante na direção dos seus sonhos. Viva a vida que imaginar."
+      break
+    case 3:
+      screen2.querySelector("p").innerText = 
+        "Faça da alegria a maior certeza da sua vida, e o tempo se encarregará de permitir que grandes momnetos te aconteçam todos os dias."
+      break
+    case 4:
+      screen2.querySelector("p").innerText = 
+        "Bom dia! Imdependentemente do que estiver sentindo, levante-se, vista-se e saia para brilhar."
+      break
+    case 5:
+      screen2.querySelector("p").innerText = 
+        "Acredite no seu valor e prove a você mesmo que todos os sonhos são possiveis de alcançar."
+      break
+    case 6:
+      screen2.querySelector("p").innerText = 
+        "Sinta carinho, goste, adore e ame! Mas, jamais precise de alguém para te fazer feliz, além de você mesmo."
+      break
+    case 7:
+      screen2.querySelector("p").innerText = 
+        "No tempo certo, tudo dará certo."
+      break
+    case 8:
+      screen2.querySelector("p").innerText = 
+        "Atitude é uma coisa pequena que faz toda a diferença."
+      break
+    case 9:
+      screen2.querySelector("p").innerText = 
+        "A persistenência pode transformar um fracasso em um sucesso extraordinário!"
+      break
+    case 10:
+      screen2.querySelector("p").innerText = 
+        "Não esiste um caminho para a felicidade. A felicidade é o caminho."
+      break
+    default:
+      break
+  }
+}
+
+function toggleScreen() {
+  screen1.classList.toggle("hide")
+  screen2.classList.toggle("hide")
+}
+
+function handleEnterclick() {
+  if (e.key == "Enter" && screen2.classList.contains("hide")) {
+    handleClick()
+  } else if (e.key == "Enter" && screen1.classList.contains("hide")) {
+    handleClick()
+  }
+}
